0f982a835b36dfce72f4ae96f28a470060535765
Your Name <you@example.com>
2020-02-11 15:29:04 -0500

    add input validation and count hidden files

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,5 +1,5 @@
 result=0
-file_count=$(ls . | wc -l)
+file_count=$(ls -a . | wc -l)
 
 validate_number () {
   if [ $1 -gt $file_count ]; then
@@ -16,8 +16,12 @@ echo "Wellcome to guessinggame!"
 
 while [ $result -eq 0 ]; do
   read -p "Guess number: " number
-  echo $number 
-  validate_number $number
+  regex='^[0-9]+$'
+  if [[ $number =~ $regex ]]; then 
+    validate_number $number
+  else
+    echo "Invalid input! You should type an integer number."
+  fi
 done
 
 
